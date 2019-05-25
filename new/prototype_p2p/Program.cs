bb77ed57fdf4eca432ce609edf03613267e0b772
areopa <36472947+areopa@users.noreply.github.com>
2019-05-25 20:42:15 +0200

    Working application

--- new/prototype_p2p/Program.cs
+++ new/prototype_p2p/Program.cs
@@ -3,21 +3,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace prototype_p2p
 {
     class Program
     {
         public static int NetworkPort = 0;
+        public static Server ServerInstance = null;
+        public static Client ClientInstance = new Client();
+        public static Chain ProjectD = new Chain();
+        public static string NodeName = "Unknown";
 
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            ProjectD.SetupChain();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            if (args.Length >= 1)
+                NetworkPort = int.Parse(args[0]);
+            if (args.Length >= 2)
+                NodeName = args[1];
+
+            if (NetworkPort > 0)
+            {
+                ServerInstance = new Server();
+                ServerInstance.Initialize();
+            }
+            if (NodeName != "Unkown")
+            {
+                Console.WriteLine($"Your name is {NodeName}");
+            }
+
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("1. Setup a connection with a server");
+            Console.WriteLine("2. Add data to chain");
+            Console.WriteLine("3. Display records");
+            Console.WriteLine("4. Exit the program");
+            Console.WriteLine("--------------------------------------");
+
+            int instruction = 0;
+            while (instruction != 4)
+            {
+                switch (instruction)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the URL of the server");
+                        string serverURL = Console.ReadLine();
+                        ClientInstance.Handshake($"{serverURL}/Chain");
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the name of the server");
+                        string receiverName = Console.ReadLine();
+                        Console.WriteLine("Type you message");
+                        string data = Console.ReadLine();
+                        ProjectD.CreateMessage(new Message(NodeName, receiverName, data));
+                        ProjectD.ProcessMessageQueue(NodeName);
+                        ClientInstance.SendToAll(JsonConvert.SerializeObject(ProjectD));
+                        break;
+                    case 3:
+                        Console.WriteLine("Chain");
+                        Console.WriteLine(JsonConvert.SerializeObject(ProjectD, Formatting.Indented));
+                        break;
+
+                }
+
+                Console.WriteLine("Choose something from the instruction list");
+                string action = Console.ReadLine();
+                instruction = int.Parse(action);
+            }
+
+            ClientInstance.Exit();
         }
     }
 }
--- new/prototype_p2p/Server.cs
+++ new/prototype_p2p/Server.cs
@@ -1,1 +1,52 @@
-﻿
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace prototype_p2p
+{
+    public class Server : WebSocketBehavior
+    {
+        bool Synchronized = false;
+        WebSocketServer ServerInstance = null;
+
+        public void Initialize()
+        {
+            ServerInstance = new WebSocketServer($"ws://127.0.0.1:{Program.NetworkPort}");
+            ServerInstance.AddWebSocketService<Server>("/Chain");
+            ServerInstance.Start();
+            Console.WriteLine($"Server initialized at ws://127.0.0.1:{Program.NetworkPort}");
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            if (e.Data == "Handshake to server")
+            {
+                Console.WriteLine(e.Data);
+                Send("Handshake to client");
+            }
+            else
+            {
+                Chain newChain = JsonConvert.DeserializeObject<Chain>(e.Data);
+
+                if (newChain.CheckIntegrity() && newChain.ChainList.Count > Program.ProjectD.ChainList.Count)
+                {
+                    List<Message> messagesToAdd = new List<Message>();
+                    messagesToAdd.AddRange(newChain.MessageQueue);
+                    messagesToAdd.AddRange(Program.ProjectD.MessageQueue);
+
+                    newChain.MessageQueue = messagesToAdd;
+                    Program.ProjectD = newChain;
+                }
+
+                if (!Synchronized)
+                {
+                    Send(JsonConvert.SerializeObject(Program.ProjectD));
+                    Synchronized = true;
+                }
+            }
+        }
+    }
+}
