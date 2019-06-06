046ad75b2bbb17ecd186d9a42c77a30f35bcf311
Tahsin Kaya <0929461@hr.nl>
2019-06-06 12:02:47 +0200

    Keyss tahsin

--- C#/prototype_p2p/Block.cs
+++ C#/prototype_p2p/Block.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.Security.Cryptography;
 
+
 namespace prototype_p2p
 {
     public class Block
--- C#/prototype_p2p/Client.cs
+++ C#/prototype_p2p/Client.cs
@@ -1,8 +1,5 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using WebSocketSharp;
 
 namespace prototype_p2p
 {
